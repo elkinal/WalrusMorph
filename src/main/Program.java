65344618f45995d0f13e7d6010c67fc9ceeb54ca
alxye <elkinal@tonbridge-school.org>
2018-04-14 14:35:36 +0000

    Morphs images and saves them with a random file name

--- src/main/Program.java
+++ src/main/Program.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by alxye on 14-Apr-18.
@@ -19,12 +20,14 @@ public class Program {
     private static Path imageToInsert = Paths.get("C:\\Users\\alxye\\Pictures\\control_image.jpg");
     private static Path currentDirectory = Paths.get("C:\\Users\\alxye\\Pictures\\");
     private static BufferedImage slaveImage;
-    private Graphics graphics;
+    private static BufferedImage masterImage;
+    private static BufferedImage newImage;
 
     public static void main(String[] args) {
+        Random random = new Random();
         ArrayList<File> list = new ArrayList<>();
         try {
-            BufferedImage masterImage = ImageIO.read(imageToInsert.toFile());
+            masterImage = ImageIO.read(imageToInsert.toFile());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,9 +54,32 @@ public class Program {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
             int slaveImageWidth = slaveImage.getWidth();
             int slaveImageHeight = slaveImage.getHeight();
-            System.out.println(file + " " + slaveImageHeight + "x" + slaveImageWidth);
+            int masterImageHeight = masterImage.getHeight();
+            int masterImageWidth = masterImage.getWidth();
+
+            int w = Math.max(slaveImageWidth, masterImageWidth);
+            int h = Math.max(slaveImageHeight, masterImageHeight);
+
+            newImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+            Graphics g = newImage.getGraphics();
+
+            if (slaveImage.getWidth() < masterImage.getWidth()) {
+                g.drawImage(masterImage, 0, 0, null);
+                g.drawImage(slaveImage, 0, 0, null);
+            }
+            else {
+                g.drawImage(slaveImage, 0, 0, null);
+                g.drawImage(masterImage, 0, 0, null);
+            }
+            try {
+                ImageIO.write(newImage, "JPG", new File(String.valueOf(currentDirectory + "\\convertedImages"), random.nextInt(100000) + "snippet.jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
