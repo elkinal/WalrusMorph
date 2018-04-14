05f3b5858579f0b12d705df6e0626e4c7ab9d48a
alxye <elkinal@tonbridge-school.org>
2018-04-14 14:09:33 +0000

    Creates a directory that will contain the edited images

--- src/main/Program.java
+++ src/main/Program.java
@@ -13,7 +13,10 @@ import java.util.ArrayList;
 public class Program {
 
     //private static Path currentDirectory = Paths.get(System.getProperty("user.dir"));
+    private static Path imageToInsert = Paths.get("C:\\Users\\alxye\\Pictures\\control_image.jpg");
     private static Path currentDirectory = Paths.get("C:\\Users\\alxye\\Pictures\\");
+
+
     public static void main(String[] args) {
         ArrayList<File> list = new ArrayList<>();
         Path path2 = Paths.get(currentDirectory + "a specific image");
@@ -23,6 +26,18 @@ public class Program {
                     .forEach(z -> list.add(z.toFile()));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        try {
+            //make sure the images are stored in an existing directory
+            if (Files.notExists(Paths.get(currentDirectory + "\\convertedImages"))) {
+                Files.createDirectories(Paths.get(currentDirectory + "\\convertedImages"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(File file : list) {
+            System.out.println(file);
         }
     }
 }
