0d962c0d6f7e1394095278b46001f4d408c09316
alxye <elkinal@tonbridge-school.org>
2018-04-14 13:55:28 +0000

    Lists all the files in Pictures

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,10 +1,28 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  * Created by alxye on 14-Apr-18.
  */
 public class Program {
+
+    //private static Path currentDirectory = Paths.get(System.getProperty("user.dir"));
+    private static Path currentDirectory = Paths.get("C:\\Users\\alxye\\Pictures\\");
     public static void main(String[] args) {
-        
+        ArrayList<File> list = new ArrayList<>();
+        Path path2 = Paths.get(currentDirectory + "a specific image");
+        try {
+            Files.walk(currentDirectory)
+                    .filter(p -> p.toString().endsWith(".jpg"))
+                    .forEach(z -> list.add(z.toFile()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
